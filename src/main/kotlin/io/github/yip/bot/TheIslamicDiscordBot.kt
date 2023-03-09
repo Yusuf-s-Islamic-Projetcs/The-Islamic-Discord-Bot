80b734533d30de21d9c6d08cdcfc5b40dcba4fd8
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-03-09 17:38:17 +0000

    Update TheIslamicDiscordBot.kt

--- src/main/kotlin/io/github/yip/bot/TheIslamicDiscordBot.kt
+++ src/main/kotlin/io/github/yip/bot/TheIslamicDiscordBot.kt
@@ -1,4 +1,14 @@
 package io.github.yip.bot
 
-class TheIslamicDiscordBot {
+import io.github.realyusufismail.jconfig.JConfig
+import io.github.realyusufismail.jconfig.util.JConfigUtils
+import io.github.ydwk.ydwk.BotBuilder.createDefaultBot
+import io.github.ydwk.ydwk.YDWK
+
+val config : JConfig = JConfig.build()
+
+fun main() {
+    val ydwk : YDWK = createDefaultBot(config.get("token").asString)
+        .build()
 }
+
